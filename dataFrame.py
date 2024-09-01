3bb6107459e330c1f0a86ddb58099e19219b6c5e
CanYouNotMan <nickha401@gmail.com>
2024-09-01 14:55:25 -0700

    Add files via upload

---
+++ dataFrame.py
@@ -0,0 +1,13 @@
+# Importing the install pandas and the package Ipython.display
+
+import pandas as pd
+
+dict = {'Name' :['Martha', 'Tim', 'Rob'],
+    'MAth' :[88,87,56],
+    'English' :[45,26,35]
+}
+df = pd.DataFrame(dict)
+
+
+
+
